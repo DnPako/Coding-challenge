d391f104dc32c2de3f84948b1af1b002aafd824f
Mouad MASTOUR <mouadmastour@gmail.com>
2017-09-21 00:40:29 +0100

    Add components files and Semantic library

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Footer = () => {
+    render() {
+        return (
+            <footer></footer>
+        )
+    }
+}
+
+
+export default Footer;
---
+++ src/components/Map.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Map = () => {
+    render() {
+        return (
+            <div>
+                map
+            </div>
+        )
+    }
+}
+
+
+export default Map;
